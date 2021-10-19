f84ab0e3e94f823ea0b16f5f4a6c15a1de6f1f4f
Evyn Newton <newton.evyn@gmail.com>
2021-10-18 21:48:10 -0400

    updated logo and header

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,24 +1,35 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/newLogo.png";
 
 const useStyles = makeStyles((theme) => ({
   logo: {
-    maxWidth: "200px",
+    maxWidth: "300px",
     alignSelf: "center",
-    margin: "3vh 0 3vh 0",
+    margin: "1vh 0 1vh 0",
     zIndex: "50",
+    // backgroundColor: "white",
+  },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    width: "100vw",
+    backgroundColor: "white",
+    // height: "10vh",
+    marginBottom: "5vh",
   },
 }));
 
 export default function Logo({ width }) {
   const classes = useStyles();
   return (
-    <img
-      className={classes.logo}
-      width={width ? width : classes.logo.width}
-      src={logo}
-      alt="logo"
-    />
+    <div className={classes.container}>
+      <img
+        className={classes.logo}
+        width={width ? width : classes.logo.width}
+        src={logo}
+        alt="logo"
+      />
+    </div>
   );
 }
